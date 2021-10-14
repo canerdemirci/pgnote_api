e0cc24d89b7b43661b85f23306879eac88ad1e1e
Caner Demirci <caner.demirci@outlook.com>
2021-10-14 12:57:38 +0300

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-    database: process.env.DB_NAME
+    host: process.env.DEVOLEPMENT ? process.env.DEV_DB_HOST : process.env.DB_HOST,
+    user: process.env.DEVOLEPMENT ? process.env.DEV_DB_USER : process.env.DB_USER,
+    password: process.env.DEVOLEPMENT ? process.env.DEV_DB_PASSWORD : process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DEVOLEPMENT ? process.env.DEV_DB_NAME : process.env.DB_NAME
 });
 
 module.exports = pool;
